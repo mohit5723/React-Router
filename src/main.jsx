944e003b6705ab6bbb6c351c6ab82acd9b0d4766
Mohitpuri Goswami <mohitgoswami379@gmail.com>
2023-10-22 20:56:11 +0530

    react router

---
+++ src/main.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/home/Home.jsx'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import User from './components/user/User'
+import Github from './components/github/Github'
+
+// const router = createBrowserRouter([
+//   {
+//     // top level element where nesting happens
+//     path:'/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path:"",
+//         element: <Home />
+//       },
+//       {
+//         path:"about",
+//         element: <About />
+//       },
+//       {
+//         path:"contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />} >
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About />}/>
+      <Route path='contact' element={<Contact />}/>
+      <Route path='user/:userid' element={<User />}/>
+      <Route
+       loader={githubInfoLoader}
+       path='github' element={<Github />}/>
+
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
